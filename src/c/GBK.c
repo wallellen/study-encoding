4b02550c72294ce63ae40a3d4f8805d92f40a123
alvin <mousebaby8080@gmail.com>
2015-06-04 00:30:30 +0800

    add latin encoding

--- src/c/GBK.c
+++ src/c/GBK.c
@@ -2,22 +2,22 @@
 
 int main(int argc, char* argv[]) {
     int i, j;
-    FILE* pf = fopen("gb2312_table.txt", "wb");
+    FILE* pf = fopen("GBK_table.txt", "wb");
     if (pf == NULL) {
         perror("Error cased, can not open file\n");
         return 1;
     }
 
-    for (i = 1; i <= 87; i++) {
+    for (i = 1; i <= 119; i++) {
         if (i > 1) {
             fputs("\n\n", pf);
         }
         fprintf(pf, "--------------------------------------------%d------------------------------------------------\n", i);
         for (j = 1; j <= 94; j++) {
+            char s[] = {(char)(0x80 + i), (char)(0xA0 + j), 0};
             if (j % 50 == 0) {
                 fputs("\n", pf);
             }
-            char s[] = {(char)(0xA0 + i), (char)(0xA0 + j), 0};
             fputs(s, pf);
         }
     }
---
+++ src/c/ISO8859.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    int i;
+    FILE* pf = fopen("ISO8859.txt", "wb");
+    if (pf == NULL) {
+        perror("Error cased, can not open file\n");
+        return 1;
+    }
+    for (i = 1; i < 0xFF; i++) {
+        char c[] = {i, '\0'};
+        if (i % 50 == 0) {
+            fputs("\n", pf);
+        }
+        fputs(c, pf);
+    }
+    printf("Work finish\n");
+    return 0;
+}
